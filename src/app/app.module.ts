fe5837d5cc54ed8a989df7aefb3586c516ec9a01
KaiserKatze <donizyo@gmail.com>
2018-11-05 20:52:54 +0800

    Migrate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 
 @NgModule({
