b68806f3d6157cd7d0d65b735007343fc28fa77b
KaiserKatze <donizyo@gmail.com>
2018-11-05 21:24:34 +0800

    Update module

--- src/app/modules/ng-bootstrap.module.ts
+++ src/app/modules/ng-bootstrap.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    NgbModule,
   ]
 })
 export class NgBootstrapModule { }
