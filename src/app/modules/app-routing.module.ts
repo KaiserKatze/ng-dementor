c1103b7ad4fd13b88e3d8406d8c6d3a8e22d2c90
KaiserKatze <donizyo@gmail.com>
2018-11-08 23:40:22 +0800

    Add module

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: false, }
+    ),
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+  ],
 })
 export class AppRoutingModule { }
