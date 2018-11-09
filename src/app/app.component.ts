f40854cde895b5aaff65c71d5504c1d86e82fa8a
KaiserKatze <donizyo@gmail.com>
2018-11-09 21:44:17 +0800

    Update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  copyright = {
+    from: 2018,
+    period: '',
+    claimant: 'KaiserKatze',
+  };
+
+  constructor() {
+    let from: number = this.copyright.from;
+    let now: Date = new Date();
+    let toyear: number = now.getFullYear();
+    if (toyear > from) {
+      this.copyright.period = `${from}-${toyear}`;
+    } else {
+      this.copyright.period = from.toString();
+    }
+  }
 }
