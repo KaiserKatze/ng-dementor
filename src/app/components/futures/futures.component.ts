aa471bff1fd0ff0957ce69e13b52502318e53caa
KaiserKatze <donizyo@gmail.com>
2018-11-14 23:45:08 +0800

    Update component

--- src/app/components/futures/futures.component.ts
+++ src/app/components/futures/futures.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TargetComponent } from '../target.component';
+
 @Component({
   selector: 'app-futures',
   templateUrl: './futures.component.html',
   styleUrls: ['./futures.component.css']
 })
-export class FuturesComponent implements OnInit {
+export class FuturesComponent extends TargetComponent implements OnInit {
 
-  constructor() { }
+  scrapystate = 0;
+
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
