ac6f013e2b2296187e8df243559634aece103a68
KaiserKatze <donizyo@gmail.com>
2018-11-15 20:51:54 +0800

    Update module

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FuturesComponent } from '../components/futures/futures.component';
 import { CurrenciesComponent } from '../components/currencies/currencies.component';
-
+import { TargetResolverService } from '../modules/target-resolver.service';
 
 export const routes: Routes = [{
   path: 'futures',
   component: FuturesComponent,
+  resolve: TargetResolverService,
 }, {
   path: 'currencies',
   component: CurrenciesComponent,
