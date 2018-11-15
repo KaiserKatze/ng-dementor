fb36f074884b6213459ec81520997ac41c1998c8
KaiserKatze <donizyo@gmail.com>
2018-11-16 00:14:49 +0800

    Update service

--- src/app/modules/target-resolver.service.ts
+++ src/app/modules/target-resolver.service.ts
@@ -1,10 +1,49 @@
 import { Injectable } from '@angular/core';
+import {
+  Router,
+  Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot,
+} from '@angular/router';
+
+import { Observable, of, EMPTY }  from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+import { Target } from '../models/target.model';
+import { TargetCollectorService } from './target-collector.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TargetResolverService {
+export class TargetResolverService implements Resolve<Target> {
 
-  constructor() {}
+  constructor(
+    private collector: TargetCollectorService,
+    private router: Router,
+  ) {}
+
+  navigate(): void {
+    const url = '/';
+    this.router.navigate([url]);
+  }
+
+  resolve(route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot): Observable<Target> | Observable<never> {
+    const name = route.paramMap.get('name');
+
+    return this.collector.getTarget(name).pipe(
+
+      mergeMap(target => {
+        if (target) {
+          return of(target);
+        } else {
+          // name not found
+          this.navigate();
+          return EMPTY;
+        }
+      }),
+
+    );
+  }
 
 }
