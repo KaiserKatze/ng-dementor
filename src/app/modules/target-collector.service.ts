7bbc8c3f00dd3d91e541a8486761264201828f67
KaiserKatze <donizyo@gmail.com>
2018-11-16 00:14:25 +0800

    Update service

--- src/app/modules/target-collector.service.ts
+++ src/app/modules/target-collector.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+} from '@angular/common/http';
+
+import { Observable, EMPTY }  from 'rxjs';
+
+import { Target } from '../models/target.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TargetCollectorService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  getTarget(name: string): Observable<Target> {
+    return EMPTY;
+  }
+
 }
